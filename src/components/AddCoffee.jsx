0df57f3ef5a55feefa201e780f13950ec2323b12
MD Enjamul Islam <mdenjamulislam527@gmail.com>
2024-05-15 12:24:04 +0600

    Add coffee in database

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,53 +1,90 @@
 import { Link } from "react-router-dom";
 import { FaArrowLeftLong } from "react-icons/fa6";
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 const AddCoffee = () => {
+    const handleAddCoffee = (e) => {
+        e.preventDefault();
+        const form = e.target;
+
+        const coffeeName = form.coffee_name.value;
+        const chef = form.chef.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photoUrl = form["photo-url"].value;
+
+        const coffee = { coffeeName, chef, supplier, taste, category, details, photoUrl };
+        console.log(coffee);
+
+        // Send the coffee object to the server
+        fetch("http://localhost:5000/coffee", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(coffee)
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                if(data.insertedId) {
+                    toast.success("Coffee added successfully");
+                    form.reset();
+                }
+            });
+    };
     return (
         <div className="container pb-10 md:pb-16 lg:pb-24">
             <div className="py-8 md:py-10 lg:py-12">
                 <Link to="/" className="inline-flex items-center gap-4 text-primary hover:text-accent">
                     <FaArrowLeftLong className="text-xl md:text-3xl lg:text-4xl" />
-                    <span className="text-xl md:text-3xl lg:text-4xl font-bold font-rancho">Back To Home</span>
+                    <span className="font-rancho text-xl font-bold md:text-3xl lg:text-4xl">Back To Home</span>
                 </Link>
             </div>
 
-            <div className="bg-light space-y-6 p-4 md:space-y-8 md:px-12 md:py-10 lg:px-28 lg:py-16">
+            <div className="space-y-6 bg-light p-4 md:space-y-8 md:px-12 md:py-10 lg:px-28 lg:py-16">
                 <div className="space-y-4 text-center lg:px-20">
-                    <h1 className="font-rancho font-bold text-center text-xl text-primary md:text-3xl lg:text-5xl">Add New Coffee</h1>
+                    <h1 className="text-center font-rancho text-xl font-bold text-primary md:text-3xl lg:text-5xl">Add New Coffee</h1>
                     <p className="text-text-primary">It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
                 </div>
-                <form className="grid grid-cols-1 gap-5 md:grid-cols-2">
+                <form onSubmit={handleAddCoffee} className="grid grid-cols-1 gap-5 md:grid-cols-2">
                     <div className="space-y-2.5">
-                        <label className="text-text-primary block text-base font-medium md:text-lg">Name</label>
-                        <input type="text" name="coffee-name" placeholder="Enter coffee name" className="form-field" />
+                        <label className="block text-base font-medium text-text-primary md:text-lg">Name</label>
+                        <input type="text" name="coffee_name" placeholder="Enter coffee name" className="form-field" />
                     </div>
                     <div className="space-y-2.5">
-                        <label className="text-text-primary block text-base font-medium md:text-lg">Chef</label>
+                        <label className="block text-base font-medium text-text-primary md:text-lg">Chef</label>
                         <input type="text" name="chef" placeholder="Enter coffee chef" className="form-field" />
                     </div>
                     <div className="space-y-2.5">
-                        <label className="text-text-primary block text-base font-medium md:text-lg">Supplier</label>
+                        <label className="block text-base font-medium text-text-primary md:text-lg">Supplier</label>
                         <input type="text" name="supplier" placeholder="Enter coffee supplier" className="form-field" />
                     </div>
                     <div className="space-y-2.5">
-                        <label className="text-text-primary block text-base font-medium md:text-lg">Taste</label>
+                        <label className="block text-base font-medium text-text-primary md:text-lg">Taste</label>
                         <input type="text" name="taste" placeholder="Enter coffee taste" className="form-field" />
                     </div>
                     <div className="space-y-2.5">
-                        <label className="text-text-primary block text-base font-medium md:text-lg">Category</label>
+                        <label className="block text-base font-medium text-text-primary md:text-lg">Category</label>
                         <input type="text" name="category" placeholder="Enter coffee category" className="form-field" />
                     </div>
                     <div className="space-y-2.5">
-                        <label className="text-text-primary block text-base font-medium md:text-lg">Details</label>
+                        <label className="block text-base font-medium text-text-primary md:text-lg">Details</label>
                         <input type="text" name="details" placeholder="Enter coffee details" className="form-field" />
                     </div>
                     <div className="col-span-full space-y-2.5">
-                        <label className="text-text-primary block text-base font-medium md:text-lg">Photo</label>
+                        <label className="block text-base font-medium text-text-primary md:text-lg">Photo</label>
                         <input type="url" name="photo-url" placeholder="Enter photo url" className="form-field" />
                     </div>
-                    <button type="submit" className="w-full col-span-full btn--filled">Add Coffee</button>
+                    <button type="submit" className="btn--filled col-span-full w-full">
+                        Add Coffee
+                    </button>
                 </form>
             </div>
+            <ToastContainer />
         </div>
     );
 };
